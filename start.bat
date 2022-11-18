5d18647787c084bb887fd86f5e22f8b20003d414
dattoMotto <118487043+dattoMotto@users.noreply.github.com>
2022-11-18 16:31:32 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 start "" https://jeopardy.jacobmartinq.repl.co/ #open for multiplayer 
-pip install -r requirements.txt
+pip install -r requirements.txt #install required libs
 cd "assets"
 call debug.py #debug for stable connection! :) 
