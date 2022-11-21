9118964335c85d35dcfd2bbbeda37d760073d3c8
dattoMotto <118487043+dattoMotto@users.noreply.github.com>
2022-11-20 22:14:31 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
-start "Jeopardy" cmd /k "echo Connecting please wait... & cd assets & call connect.py && echo Connected! & pause &  start "" https://jeopardy.jacobmartinq.repl.co/ && exit"
+start "Jeopardy" cmd /k "echo Verifying system requirements... && pip install -r requirements.txt && CLS && echo Connecting please wait... && cd assets & call connect.py && echo Connected! && pause && start "" https://jeopardy.jacobmartinq.repl.co/ && exit"
 exit
 
